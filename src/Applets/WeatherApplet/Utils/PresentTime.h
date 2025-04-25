8d2771accc190e74c9b16635d2bdfe35d05a393b
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2025-04-25 17:45:07 +0200

    chore: update copyright notice to 2025

--- src/Applets/WeatherApplet/Utils/PresentTime.h
+++ src/Applets/WeatherApplet/Utils/PresentTime.h
@@ -1,6 +1,6 @@
 /* Pretty Applets
 
-   Copyright (C) 2024 Łukasz Wrodarczyk
+   Copyright (C) 2025 Łukasz Wrodarczyk
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
