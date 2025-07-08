5c7796f9bc15dd9186eb85c7ac740f4e6374746a
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2025-07-08 19:49:25 +0200

    refactor: add epoch_duration
    - Add epoch_duration namespace holding common epoch duration constants.

---
+++ src/Utils/TimeConstants.h
@@ -0,0 +1,32 @@
+/* Pretty Applets
+
+   Copyright (C) 2025 Łukasz Wrodarczyk
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+#ifndef TIME_CONSTANTS_H
+#define TIME_CONSTANTS_H
+
+#include <ctime>
+
+namespace epoch_duration {
+constexpr time_t minute = 60;
+constexpr time_t hour = minute * 60;
+constexpr time_t day = hour * 24;
+constexpr time_t week = day * 7;
+
+// Months and more are ommitted due to variable lengths (e.g. leap years, month lengths)
+}
+
+#endif // TIME_CONSTANTS_H
