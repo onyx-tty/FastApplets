2a54217b619d1930fe50f452a6c3b00b18d2caaf
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2025-07-15 15:05:21 +0200

    style: rearrange inclusions in OpenWeatherAPI

--- src/Applets/WeatherApplet/API/OpenWeatherAPI.cpp
+++ src/Applets/WeatherApplet/API/OpenWeatherAPI.cpp
@@ -15,9 +15,9 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 
-#include "../../../Config/WeatherLayout.h"
-#include "../../../API/Curl.h"
 #include "OpenWeatherAPI.h"
+#include "../../../API/Curl.h"
+#include "../../../Config/WeatherLayout.h"
 
 #include <QApplication>
 #include <QDebug>
