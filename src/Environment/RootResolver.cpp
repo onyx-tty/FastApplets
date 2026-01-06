97a56a87957b19ab4a8fdaf94beb12a07aad249a
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-01-06 02:15:05 +0100

    chore(Environment): update copyrights

--- src/Environment/RootResolver.cpp
+++ src/Environment/RootResolver.cpp
@@ -1,6 +1,6 @@
 /* FastApplets
 
-   Copyright (C) 2025 Łukasz Wrodarczyk
+   Copyright (C) 2026 Łukasz Wrodarczyk
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
