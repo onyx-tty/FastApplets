3412fba488cbf440a47f93ca6e967890ff48e771
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-02-05 23:01:08 +0100

    refactor(Log): extract logging utils to CppUtils

--- src/Core/Log.h
+++ src/Core/Log.h
@@ -17,59 +17,6 @@
 
 #pragma once
 
-#include <QDebug>
-#include <QString>
+#include "CppUtils/include/Qt/Log.h"
 
-namespace log_internals {
-
-template<typename T>
-QString toQString(const T& value);
-
-// Concatenate all arguments into one string
-inline QString concatArgs();
-
-template<typename MainStr, typename... RestStr>
-inline QString concatArgs(const MainStr& main_string, const RestStr&... rest_strings);
-
-} // namespace log_internals
-
-// TODO Log not only function names but also potential classes they belong to and perhaps even file names
-// TODO Create versions with formatting
-// Logging formatters
-#define QFATAL(...) do { \
-        const QString msg = log_internals::concatArgs(__VA_ARGS__); \
-        qFatal("[FATAL]    %s : %s", __func__, qPrintable(msg)); \
-} while(0)
-
-#define QCRITICAL() \
-        qCritical() << "[CRITICAL]" << __func__ << ":"
-
-#define QWARNING() \
-        qWarning() << "[WARNING]  " << __func__ << ":"
-
-// TODO Optimize, this still instantiates a QDebug object
-#ifdef LOG_DEBUG
-        #define QDEBUG() \
-                qDebug() << "[DEBUG]   " << __func__ << ":"
-#else // Swallow anything streamed to it and ignore semicolons
-        #define QDEBUG() \
-                if (false) qDebug()
-#endif
-
-#define QINFO() \
-        qInfo() << "[INFO]    " << __func__ << ":"
-
-// Logging formatters - nospace version
-#define QCRITICAL_NS() \
-        qCritical().nospace() << "[CRITICAL]" << __func__ << " : "
-
-#define QWARNING_NS() \
-        qWarning().nospace() << "[WARNING]  " << __func__ << " : "
-
-#define QDEBUG_NS() \
-        qDebug().nospace() << "[DEBUG]   " << __func__ << " : "
-
-#define QINFO_NS() \
-        qInfo().nospace() << "[INFO]    " << __func__ << " : "
-
-#include "Log.tpp"
+using namespace qt::log_utils;
