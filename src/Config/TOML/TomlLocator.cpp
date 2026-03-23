onyx-tty/FastApplets

commit fadd0664bea470602da4eab0d02e33dfdb6a4802

feat(Config)!: restrict configs to $XDG_CONFIG_HOME for simplicity
Restrict configs to $XDG_CONFIG_HOME because keeping two separate
envvars in mind made the code more complicated and more likely to fail
due to a more complex algorithm.

$XDG_CONFIG_HOME is intuitively the right directory.

BREAKING CHANGES: Configs under $XDG_DATA_HOME/FastApplets will fail
                  detection from now on.

src/Config/CMakeLists.txt
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.16)
 
-project(Config VERSION 0.17.6 LANGUAGES CXX)
+project(Config VERSION 0.18.0 LANGUAGES CXX)
 
 # Enable support for Qt's resource files
 set(CMAKE_AUTORCC ON)

src/Config/TOML/TomlLocator.cpp
@@ -27,10 +27,6 @@
 // Values needed to find configs
 namespace {
 
-static std::array<QString, 2> toml_dir_paths = {qEnvironmentVariable("XDG_CONFIG_HOME")
-                                                        + "/FastApplets/",
-                                                qEnvironmentVariable("XDG_DATA_HOME")
-                                                        + "/FastApplets/"};
 static std::array<QString, toml_file_names_cnt> config_file_names = {"config.toml", "keys.toml"};
 
 } // namespace
@@ -42,24 +38,18 @@ std::array<std::string, toml_file_names_cnt> TomlLocator::locateTomlFiles() {
 
         QString file_path;
         // Loop through expected config files
-        for (size_t file_i = 0; file_i != toml_file_names_cnt; ++file_i) {
+        // No valid file_path found for the current file, terminate
+        // TODO Fallback into default config
+        for (size_t i = 0; i != toml_file_names_cnt; ++i) {
                 bool found = false;
-                // Loop through expected directories
-                for (size_t dir_i = 0; !found && dir_i != toml_dir_paths_cnt; ++dir_i) {
-                        file_path = toml_dir_paths[dir_i] + config_file_names[file_i];
-                        // If file found, save filepath, stop the loop for that file
-                        if (QFileInfo::exists(file_path)) {
-                                files[file_i] = file_path.toStdString();
-                                found         = true;
-                                break;
-                        }
+                file_path  = qEnvironmentVariable("XDG_CONFIG_HOME") + "/FastApplets/";
+                // If file found, save filepath
+                if (QFileInfo::exists(file_path)) {
+                        files[i] = file_path.toStdString();
+                        found    = true;
                 }
 
-                // No valid file_path found for the current file, terminate
-                // TODO Generate default TOML file on failure
-                if (!found) {
-                        QFATAL("%s not found!", config_file_names[file_i].toStdString().c_str());
-                }
+                if (!found) { QFATAL("%s not found!", file_path); }
         }
 
         return files;

src/Config/TOML/TomlLocator.h
@@ -21,7 +21,6 @@
 #include <string>
 
 constexpr int toml_file_names_cnt = 2;
-constexpr int toml_dir_paths_cnt  = 2;
 
 // TODO Alias for config files
 class TomlLocator final {

commit 67a5a55afce4b5761b084ddd4ab692bdded785e6

fix(Config): fix broken TOML file pathing introduced by (fadd066)
Fix an issue where configs would be entirely omitted caused by
'TomlLocator::locateTomlFiles' passing the DIRECTORY path of config
files in both cases, instead of the dedicated FILEPATH.

src/Config/CMakeLists.txt
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.16)
 
-project(Config VERSION 0.20.5 LANGUAGES CXX)
+project(Config VERSION 0.20.6 LANGUAGES CXX)
 
 # Enable support for Qt's resource files
 set(CMAKE_AUTORCC ON)

src/Config/TOML/TomlLocator.cpp
@@ -33,13 +33,14 @@ std::array<std::string, toml_file_names_cnt> TomlLocator::locateTomlFiles() {
         // Loop through expected config files
         // No valid file_path found for the current file, terminate
         // TODO Fallback into default config
-        for (auto& file : files) {
+        for (size_t i = 0; i != toml_file_names_cnt; ++i) {
                 bool found = false;
-                file_path  = qEnvironmentVariable("XDG_CONFIG_HOME") + "/FastApplets/";
+                file_path  = qEnvironmentVariable("XDG_CONFIG_HOME") + "/FastApplets/"
+                           + config_file_names[i];
                 // If file found, save filepath
                 if (QFileInfo::exists(file_path)) {
-                        file  = file_path.toStdString();
-                        found = true;
+                        files[i] = file_path.toStdString();
+                        found    = true;
                 }
 
                 if (!found) { QFATAL("%s not found!", file_path); }