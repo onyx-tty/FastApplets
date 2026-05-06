3ec96d8cccf1e1d891e4aed7ba87778d17f31388
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-05-06 23:58:12 +0200

    feat(Applets/Types): add toString() converting applet::type to string

--- src/Applets/Types/AppletRecord.h
+++ src/Applets/Types/AppletRecord.h
@@ -17,4 +17,11 @@ struct Record {
 inline constexpr Record global{type::global, "global"};
 inline constexpr Record power_applet{type::power_applet, "power_applet"};
 
+constexpr std::string_view toString(applet::type id) {
+        switch (id) {
+        case type::global:       return "global";
+        case type::power_applet: return "power_applet";
+        }
+}
+
 } // namespace applet
