928ce3524f8bf7a1eacda96870cf7b8496e9b96a
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-05-07 23:25:06 +0200

    refactor(Shell): inline singleQuotesToDoubleQuotes
    singleQuotesToDoubleQuotes() is currently a very simple and easy to
    understand helper, which doesn't warrant separation.

--- src/Shell/ShellRunner.cpp
+++ src/Shell/ShellRunner.cpp
@@ -6,14 +6,10 @@
 #include <QProcess>
 #include <QStringList>
 
-// QProcess::splitCommand treats single quotes as regular characters
-// Treat them like proper quotes by reinterpreting single quotes as double quotes
-inline void singleQuotesToDoubleQuotes(QString& command) {
-        command.replace("'", "\"");
-}
-
 void ShellRunner::runCommand(QString command) {
-        singleQuotesToDoubleQuotes(command);
+        // QProcess::splitCommand treats single quotes as regular characters
+        // Treat them like proper quotes by reinterpreting single quotes as double quotes
+        command.replace("'", "\"");
 
         QStringList parts   = QProcess::splitCommand(command);
         QString     program = parts.takeFirst();
