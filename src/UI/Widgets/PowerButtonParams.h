35a4c3c1872b19476c5b11e695ecaf485da5b2d6
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-05-07 21:13:18 +0200

    refactor(UI/Widgets): remove PrimaryButtonParams, avoid inheritance
    PrimaryButtonParams is a trivial struct, it does not need to be created
    on its own, and adding virtualization to prevent bugs is an unnecessary
    layer of abstraction. Make PowerButtonParams a separate struct.

--- src/UI/Widgets/PowerButtonParams.h
+++ src/UI/Widgets/PowerButtonParams.h
@@ -3,21 +3,16 @@
 
 #pragma once
 
-#include "UI/Widgets/PrimaryButtonParams.h"
+#include "UI/Enums/ButtonIDs.h"
 
-#include <utility>
 #include <QIcon>
 #include <QString>
 
-struct PowerButtonParams final : PrimaryButtonParams {
+struct PowerButtonParams final {
         power_button_id id;
+        QString         text;
+        long            order;
         QString         command;
         QString         dbus_method;
         QIcon           icon;
-
-        PowerButtonParams(power_button_id id = power_button_id::none, QString text = {},
-                          long order = {}, QString command = {}, QString dbus_method = {},
-                          const QIcon& icon = {}) :
-                PrimaryButtonParams(std::move(text), order), id(id), command(std::move(command)),
-                dbus_method(std::move(dbus_method)), icon(icon) {}
 };
