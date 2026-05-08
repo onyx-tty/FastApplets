f8548ebe477559fa21248597e865c9f4b36e7884
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-05-08 19:28:44 +0200

    feat(Config/Types): add config::type enum for identification

---
+++ src/Config/Types/ConfigType.h
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2026 Łukasz Wrodarczyk
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+namespace config {
+
+enum class type { config, keys };
+
+} // namespace config
