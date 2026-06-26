fd04ddbc677e753f16e3c12d4ff46c86689899a2
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-06-26 23:47:49 +0200

    docs(Core/Shell): add a TODO and a FIXME

--- Core/Shell/ShellRunner.cpp
+++ Core/Shell/ShellRunner.cpp
@@ -7,6 +7,8 @@
 #include <QString>
 #include <QStringList>
 
+// TODO: Quit and warn if empty
+// FIXME: Splitting command into QStringList is a fatal QList error if command is empty!
 void ShellRunner::command(QString command) {
         // QProcess::splitCommand treats single quotes as regular characters
         // Treat them like proper quotes by reinterpreting single quotes as double quotes
