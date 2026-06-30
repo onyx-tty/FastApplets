1ed6e0bccf0306fe07bf9304ec437a2b6221bc38
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-06-30 14:17:22 +0200

    docs(Applets/Types): document AppletTraits

--- Core/Applets/Types/AppletTraits.h
+++ Core/Applets/Types/AppletTraits.h
@@ -5,5 +5,11 @@
 
 #include "AppletType.h"
 
+// Stores generic types required for use in Core classes and functions.
+// They're used to avoid including each applet's classes in Core classes, which
+// would prevent Core from being isolated from the applets using it.
+//
+// To work, each applet must include its own AppletTraits template specializations
+// before including the classes requiring them.
 template<applet::type T>
 struct AppletTraits;
