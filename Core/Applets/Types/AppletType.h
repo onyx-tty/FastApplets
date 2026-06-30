7fb75aa01098f083c2dddb72fb31ac4a455f6663
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-06-30 21:16:07 +0200

    refactor(Applets/Types): replace assert and unreachable with qFatal
    assert is optimized away in Release builds, which would cause UB and
    prevent important information from being printed out if an error occurs.
    
    Replace the assert() and std::unreachable() combination with qFatal.

--- Core/Applets/Types/AppletType.h
+++ Core/Applets/Types/AppletType.h
@@ -3,11 +3,10 @@
 
 #pragma once
 
-#include <cassert>
-#include <utility>
 #include <QLatin1StringView>
 #include <QStringView>
 #include <Qt>
+#include <QtGlobal>
 
 namespace applet {
 
@@ -19,8 +18,7 @@ constexpr QStringView toString(type applet_type) {
         case type::global:        return u"global";
         case type::power_applet:  return u"power_applet";
         case type::action_applet: return u"action_applet";
-        // TODO: Replace assert() with qFatal()
-        default:                  assert(false && "Passed unknown type"); std::unreachable();
+        default:                  qFatal("Passed unknown type");
         }
 }
 
@@ -32,8 +30,7 @@ constexpr QLatin1StringView toLatin1String(type applet_type) {
         case type::global:        return "global"_L1;
         case type::power_applet:  return "power_applet"_L1;
         case type::action_applet: return "action_applet"_L1;
-        // TODO: Replace assert() with qFatal()
-        default:                  assert(false && "Passed unknown type"); std::unreachable();
+        default:                  qFatal("Passed unknown type");
         }
 }
 
