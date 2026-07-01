35e587c9a09d798840ff3fa311d9e7083667a29f
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-07-01 16:21:00 +0200

    feat(Shell): support shell substitution
    Previously shell substitution did not happen, so for example this:
    
    $ notify-send "Current date:" "$(date +'%F %H:%M')"
    
    would not turn into for example this:
    
    $ notify-send "Current date:" "2026-05-20 12:41"
    
    but instead would remain the same.
    
    Fix it by executing given command by '/bin/sh'. This also removes the
    need for QStringList and QProcess::splitCommand(), as well as the
    workaround reinterpreting single quotes as double quotes.

--- Core/Shell/ShellRunner.cpp
+++ Core/Shell/ShellRunner.cpp
@@ -3,21 +3,18 @@
 
 #include "Core/Shell/ShellRunner.h"
 
+#include <utility>
 #include <QDebug>
 #include <QProcess>
 #include <QString>
-#include <QStringList>
 #include <QtGlobal>
 
 void ShellRunner::command(QString command) {
-        // Avoids splitting empty command into QStringList, which is a fatal QList error
+        // Avoids passing empty command to shell, which does nothing.
         if (command.isEmpty()) {
                 qWarning() << "Passed empty command!";
                 return;
         }
 
-        QStringList parts   = QProcess::splitCommand(command);
-        QString     program = parts.takeFirst();
-
-        QProcess::startDetached(program, parts);
+        QProcess::startDetached("/bin/sh", {"-c", std::move(command)});
 }
