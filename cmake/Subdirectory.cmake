4192253dd10a573d1789c89c6c7915ef55e640ae
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-07-22 07:39:28 +0200

    build: introduce add_subdirectories for multiple subdirs in one function

---
+++ cmake/Subdirectory.cmake
@@ -0,0 +1,9 @@
+function(add_subdirectories)
+        if(NOT ARGN)
+                message(WARNING "Empty add_subdirectories() does nothing")
+        endif()
+
+        foreach(NAME ${ARGN})
+                add_subdirectory(${NAME})
+        endforeach()
+endfunction()
