254b357fbb7fad3ccc66d34f0a58a05e24133fdb
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-07-22 06:55:52 +0200

    build: reduce library boilerplate with add_applet_library()
    add_applet_library() is a function that automatically adds a library
    with given NAME, and creates an equivalent :: alias for it.
    
    TYPE can also be specified. By default it's empty.
    
    It also allows for private linking using PRIVATE_DEPS, public linking
    using PUBLIC_DEPS, and interface linking using INTERFACE_DEPS.
    
    Currently the function always includes directories by PRIVATE (it will
    be made more flexible in the future).
    
    The function relies on a pre-defined ROOT_DIR variable, and terminates
    configuration with an error if it's missing.
    
    It also guards against incorrect linking practices, such as PRIVATE or
    PUBLIC to an INTERFACE library, or non-INTERFACE to an INTERFACE
    library.

---
+++ cmake/AddAppletLibrary.cmake
@@ -0,0 +1,57 @@
+include(CMakeParseArguments)
+
+function(add_applet_library NAME)
+        set(OPTIONS)
+        set(SINGLE_VALUE_ARGS TYPE)
+        set(MULTI_VALUE_ARGS SOURCES PRIVATE_DEPS PUBLIC_DEPS INTERFACE_DEPS)
+
+        cmake_parse_arguments(ARG "${OPTIONS}"
+                                  "${SINGLE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}"
+                                  ${ARGN})
+
+        if (NOT ROOT_DIR)
+                message(FATAL_ERROR "ROOT_DIR has not been defined")
+        endif()
+
+        set(ALIAS_NAME)
+        string(REPLACE "_" "::" ALIAS_NAME ${NAME})
+
+        add_library(${NAME} ${ARG_TYPE} ${ARG_SOURCES})
+        add_library(${ALIAS_NAME} ALIAS ${NAME})
+
+        if (ARG_TYPE STREQUAL "INTERFACE")
+                target_include_directories(${NAME} INTERFACE
+                        $<BUILD_INTERFACE:${ROOT_DIR}>
+                        $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
+
+                if (ARG_INTERFACE_DEPS)
+                        target_link_libraries(${NAME} INTERFACE ${ARG_INTERFACE_DEPS})
+                endif()
+
+                if (ARG_PRIVATE_DEPS)
+                        message(FATAL_ERROR "PRIVATE deps are not allowed in INTERFACE libraries")
+                endif()
+
+                if (ARG_PUBLIC_DEPS)
+                        message(FATAL_ERROR "PUBLIC deps are not allowed in INTERFACE libraries")
+                endif()
+
+                return()
+        endif()
+
+        target_include_directories(${NAME} PRIVATE
+                $<BUILD_INTERFACE:${ROOT_DIR}>
+                $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
+
+        if (ARG_INTERFACE_DEPS)
+                message(FATAL_ERROR "INTERFACE deps are not allowed in non-INTERFACE libraries")
+        endif()
+
+        if (ARG_PRIVATE_DEPS)
+                target_link_libraries(${NAME} PRIVATE ${ARG_PRIVATE_DEPS})
+        endif()
+
+        if (ARG_PUBLIC_DEPS)
+                target_link_libraries(${NAME} PUBLIC ${ARG_PUBLIC_DEPS})
+        endif()
+endfunction()
