2b686c10c0942406de554952d1dcfd4baac95bd7
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-07-22 23:13:03 +0200

    build: introduce and use add_applet executable variants
    Introduce add_applet_executable() (for regular executables) and
    add_applet_qt_executables() (for Qt executables).

--- cmake/AddTarget.cmake
+++ cmake/AddTarget.cmake
@@ -55,3 +55,64 @@ function(add_applet_library NAME)
                 target_link_libraries(${NAME} PUBLIC ${ARG_PUBLIC_DEPS})
         endif()
 endfunction()
+
+function(add_applet_executable NAME)
+        set(OPTIONS)
+        set(SINGLE_VALUE_ARGS)
+        set(MULTI_VALUE_ARGS SOURCES PRIVATE_DEPS PUBLIC_DEPS)
+
+        cmake_parse_arguments(ARG "${OPTIONS}"
+                                  "${SINGLE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}"
+                                  ${ARGN})
+
+        if(NOT ROOT_DIR)
+                message(FATAL_ERROR "ROOT_DIR has not been defined")
+        endif()
+
+        add_executable(${NAME} ${ARG_SOURCES})
+
+        target_include_directories(${NAME} PRIVATE
+                $<BUILD_INTERFACE:${ROOT_DIR}>
+                $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
+
+        if(ARG_PRIVATE_DEPS)
+                target_link_libraries(${NAME} PRIVATE ${ARG_PRIVATE_DEPS})
+        endif()
+
+        if(ARG_PUBLIC_DEPS)
+                target_link_libraries(${NAME} PUBLIC ${ARG_PUBLIC_DEPS})
+        endif()
+endfunction()
+
+function(add_applet_qt_executable NAME)
+        set(OPTIONS MANUAL_FINALIZATION)
+        set(SINGLE_VALUE_ARGS)
+        set(MULTI_VALUE_ARGS SOURCES PRIVATE_DEPS PUBLIC_DEPS)
+
+        cmake_parse_arguments(ARG "${OPTIONS}"
+                                  "${SINGLE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}"
+                                  ${ARGN})
+
+        if(NOT ROOT_DIR)
+                message(FATAL_ERROR "ROOT_DIR has not been defined")
+        endif()
+
+        set(MANUAL_FINALIZATION)
+        if(ARG_MANUAL_FINALIZATION)
+                set(MANUAL_FINALIZATION MANUAL_FINALIZATION)
+        endif()
+
+        qt_add_executable(${NAME} ${MANUAL_FINALIZATION} ${ARG_SOURCES})
+
+        target_include_directories(${NAME} PRIVATE
+                $<BUILD_INTERFACE:${ROOT_DIR}>
+                $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
+
+        if(ARG_PRIVATE_DEPS)
+                target_link_libraries(${NAME} PRIVATE ${ARG_PRIVATE_DEPS})
+        endif()
+
+        if(ARG_PUBLIC_DEPS)
+                target_link_libraries(${NAME} PUBLIC ${ARG_PUBLIC_DEPS})
+        endif()
+endfunction()
