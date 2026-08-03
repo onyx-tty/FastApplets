08f0630c2d6e3398eb0fc373fe53b8d5d58289d8
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-08-04 00:50:31 +0200

    refactor(UI/Types): rename poweroff to power_off to match log_out
    power_off is an alternative key to deserialize power off and shutdown as
    power_button_type::shutdown. The codebase mostly uses "shutdown", but
    "power_off" is an option too.

--- Core/UI/Types/ButtonType.cpp
+++ Core/UI/Types/ButtonType.cpp
@@ -10,7 +10,7 @@
 template<>
 power_button_type toPrimaryButtonType<power_button_type>(QStringView string) {
         static const QHash<QStringView, power_button_type> map =
-                {{u"poweroff", power_button_type::shutdown},
+                {{u"power_off", power_button_type::shutdown},
                  {u"shutdown", power_button_type::shutdown},
                  {u"reboot", power_button_type::reboot},
                  {u"suspend", power_button_type::suspend},
